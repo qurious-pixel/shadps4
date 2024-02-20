9a0e739e52638776c6997369ee60428b55541beb
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-02-20 05:04:52 -0800

    Update linux-appimage-qt.sh

--- .github/linux-appimage-qt.sh
+++ .github/linux-appimage-qt.sh
@@ -10,9 +10,12 @@ echo "$PATH"
 # Prepare Tools for building the AppImage
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt-x86_64.sh
 
 chmod a+x linuxdeploy-x86_64.AppImage
 chmod a+x linuxdeploy-plugin-qt-x86_64.AppImage
+chmod a+x linuxdeploy-plugin-checkrt-x86_64.sh
 
 # Build AppImage
+./linuxdeploy-plugin-checkrt.sh --appdir AppDir
 ./linuxdeploy-x86_64.AppImage --appdir AppDir -d "$GITHUB_WORKSPACE"/.github/shadps4.desktop  -e "$GITHUB_WORKSPACE"/build/src/shadps4 -i "$GITHUB_WORKSPACE"/.github/shadps4.png --plugin qt --output appimage
