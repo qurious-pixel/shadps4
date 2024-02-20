4c6adc34c871e4d7fda8be8263282c1c8ea9cb05
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-02-19 17:53:59 -0800

    Update linux-appimage-qt.sh

--- .github/linux-appimage-qt.sh
+++ .github/linux-appimage-qt.sh
@@ -6,4 +6,4 @@ chmod a+x linuxdeploy-x86_64.AppImage
 chmod a+x linuxdeploy-plugin-qt-x86_64.AppImage
 
 # Build AppImage
-QMAKE=/usr/lib/qt6/bin/qmake ./linuxdeploy-x86_64.AppImage --appdir AppDir -d ./.github/shadps4.desktop  -e ./build/shadps4 -i SPS4.png --plugin qt --output appimage
+QMAKE=/usr/lib/qt6/bin/qmake ./linuxdeploy-x86_64.AppImage --appdir AppDir -d ./.github/shadps4.desktop  -e ./build/shadps4 -i ./build/shadps4.png --plugin qt --output appimage
