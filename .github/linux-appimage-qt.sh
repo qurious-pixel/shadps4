7affce155a52a37f0a1a8e6b77c5a0b46795d132
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-02-19 20:32:37 -0800

    Update linux-appimage-qt.sh

--- .github/linux-appimage-qt.sh
+++ .github/linux-appimage-qt.sh
@@ -4,6 +4,9 @@ if [[ -z $GITHUB_WORKSPACE ]]; then
 	GITHUB_WORKSPACE="${PWD%/*}"
 fi
 
+QMAKE6=$(find -name qmake)
+echo "$QMAKE6"
+
 # Prepare Tools for building the AppImage
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
@@ -12,4 +15,4 @@ chmod a+x linuxdeploy-x86_64.AppImage
 chmod a+x linuxdeploy-plugin-qt-x86_64.AppImage
 
 # Build AppImage
-QMAKE=/usr/lib/qt6/bin/qmake ./linuxdeploy-x86_64.AppImage --appdir AppDir -d "$GITHUB_WORKSPACE"/.github/shadps4.desktop  -e "$GITHUB_WORKSPACE"/build/src/shadps4 -i "$GITHUB_WORKSPACE"/.github/shadps4.png --plugin qt --output appimage
+QMAKE="$QMAKE6" ./linuxdeploy-x86_64.AppImage --appdir AppDir -d "$GITHUB_WORKSPACE"/.github/shadps4.desktop  -e "$GITHUB_WORKSPACE"/build/src/shadps4 -i "$GITHUB_WORKSPACE"/.github/shadps4.png --plugin qt --output appimage
